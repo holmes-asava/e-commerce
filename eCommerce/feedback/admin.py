d5fbe05ded1aee4cdeb5e6e71fb72340001b07a5
Holmes.asava <holmes@simplesat.io>
2024-03-01 17:29:01 +0700

    :sparkles: Update feedback model
    update feedback model

---
+++ eCommerce/feedback/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+
+from .models import Feedback
+
+
+@admin.register(Feedback)
+class FeedbackAdmin(admin.ModelAdmin):
+    list_display = ['id', 'score', 'type', 'subject', 'created', 'updated']
+
