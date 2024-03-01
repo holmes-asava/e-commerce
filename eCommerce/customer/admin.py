6d0607465248b158e3ec114b9167656623bb76ef
Holmes.asava <holmes@simplesat.io>
2024-03-01 17:34:55 +0700

    :sparkles: Implement admin page for customer
    implement admin page for customer

--- eCommerce/customer/admin.py
+++ eCommerce/customer/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
+from .models import Address, Customer
+
+
 # Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+
+    list_per_page = 20
+
+
+@admin.register(Address)
+class AddressAdmin(admin.ModelAdmin):
+
+    list_per_page = 20
